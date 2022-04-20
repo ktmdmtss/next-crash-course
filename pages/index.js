c17f9f2eb6ee42a117a479145c81ddfcaea46cc0
ktmdmtss <ktmbdmtss@gmail.com>
2022-04-19 22:59:04 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import ArticleList from '../components/ArticleList'
+import Head from 'next/head'
+
+export default function Home({articles}) {
+  return (
+    <div>
+      <Head>
+        <title> WebDev News</title>
+        <meta neame='keywords' content='web development, programming'/>
+      </Head>
+      <ArticleList articles={articles}/>
+    </div>
+  )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+  return {
+    props: {
+      articles
+    }
+  }
+}
